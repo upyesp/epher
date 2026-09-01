7214e71455013a3284c3e204f600877fa1af8fcb
upyesp <upyesp@gmail.com>
2026-09-01 13:43:26 +0000

    release: v0.5.12 - implicit relations

--- crates/cli/examples/gen-man.rs
+++ crates/cli/examples/gen-man.rs
@@ -28,7 +28,7 @@ fn main() {
         .title("EPHER")
         .section("1")
         .manual("User Commands")
-        .source("epher 0.5.11");
+        .source("epher 0.5.12");
     let mut out = std::io::stdout();
     man.render_title(&mut out).expect("title");
     man.render_name_section(&mut out).expect("name");
